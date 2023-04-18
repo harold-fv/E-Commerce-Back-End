01ebfe4ce485b8ae0036bee0eec4685c9d57139e
Harold Gonzales <haroldgonzales@yahoo.com>
2023-04-17 23:59:17 -0700

    Created api/index.js

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,17 +1,15 @@
 // Import Express router
 const router = require('express').Router();
 
-// Import API routes
-const apiRoutes = require('./api');
+// Import route handlers for categories, products, and tags
+const categoryRoutes = require('./category-routes');
+const productRoutes = require('./product-routes');
+const tagRoutes = require('./tag-routes');
 
-// Set up the main API route with the imported API routes
-router.use('/api', apiRoutes);
-
-// Handle requests for non-existent routes
-router.use((req, res) => {
-  res.send("<h1>Wrong Route!</h1>")
-});
+// Set up routes for categories, products, and tags with their respective route handlers
+router.use('/categories', categoryRoutes);
+router.use('/products', productRoutes);
+router.use('/tags', tagRoutes);
 
 // Export the router to be used in other parts of the application
 module.exports = router;
-
