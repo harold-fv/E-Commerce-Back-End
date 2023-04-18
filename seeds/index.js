ce8c1458fdb825ddba593d4e39b2a6a16c73f8b0
Harold Gonzales <haroldgonzales@yahoo.com>
2023-04-17 23:32:36 -0700

    Created seeds/index.js

---
+++ seeds/index.js
@@ -0,0 +1,30 @@
+// Import seed functions for each model
+const seedCategories = require('./category-seeds');
+const seedProducts = require('./product-seeds');
+const seedTags = require('./tag-seeds');
+const seedProductTags = require('./product-tag-seeds');
+
+// Import the database connection configuration
+const sequelize = require('../config/connection');
+
+// Function to seed all the data into the database
+const seedAll = async () => {
+  await sequelize.sync({ force: true });
+  console.log('\n----- DATABASE SYNCED -----\n');
+  await seedCategories();
+  console.log('\n----- CATEGORIES SEEDED -----\n');
+
+  await seedProducts();
+  console.log('\n----- PRODUCTS SEEDED -----\n');
+
+  await seedTags();
+  console.log('\n----- TAGS SEEDED -----\n');
+
+  await seedProductTags();
+  console.log('\n----- PRODUCT TAGS SEEDED -----\n');
+
+  process.exit(0);
+};
+
+// Call the seedAll function to seed the data
+seedAll();
