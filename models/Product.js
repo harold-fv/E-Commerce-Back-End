45c9e2a7d1f3dbd00a814808a2e42967b6710607
Harold Gonzales <haroldgonzales@yahoo.com>
2023-04-17 23:14:51 -0700

    Created Models/Product.js

---
+++ models/Product.js
@@ -0,0 +1,56 @@
+// Import necessary dependencies and modules
+const { Model, DataTypes } = require('sequelize');
+
+// Import the database connection configuration
+const sequelize = require('../config/connection');
+
+// Create a Product class that extends Sequelize Model
+class Product extends Model {}
+
+// Initialize Product model with its attributes and configuration options
+Product.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    product_name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    price: {
+      type: DataTypes.DECIMAL(10, 2),
+      allowNull: false,
+      validate: {
+        isDecimal: true,
+      },
+    },
+    stock: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      defaultValue: 10,
+      validate: {
+        isNumeric: true,
+      },
+    },
+    category_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'category',
+        key: 'id',
+      },
+    },
+  },
+  {
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'product',
+  }
+);
+
+// Export the Product model to be used in other parts of the application
+module.exports = Product;
