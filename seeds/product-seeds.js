f0e61e270cfebe41457bfee6794b7ddd0182810f
Harold Gonzales <haroldgonzales@yahoo.com>
2023-04-17 23:29:09 -0700

    Created seeds/product-seeds.js

---
+++ seeds/product-seeds.js
@@ -0,0 +1,42 @@
+// Import the Product model from the models folder
+const { Product } = require('../models');
+
+// Function to seed the Product table with sample data
+const seedProducts = () => Product.bulkCreate(productData);
+
+// Sample data for the Product table
+const productData = [
+  {
+    product_name: 'Plain T-Shirt',
+    price: 14.99,
+    stock: 14,
+    category_id: 1,
+  },
+  {
+    product_name: 'Running Sneakers',
+    price: 90.0,
+    stock: 25,
+    category_id: 5,
+  },
+  {
+    product_name: 'Branded Baseball Hat',
+    price: 22.99,
+    stock: 12,
+    category_id: 4,
+  },
+  {
+    product_name: 'Top 40 Music Compilation Vinyl Record',
+    price: 12.99,
+    stock: 50,
+    category_id: 3,
+  },
+  {
+    product_name: 'Cargo Shorts',
+    price: 29.99,
+    stock: 22,
+    category_id: 2,
+  },
+];
+
+// Export the seedProducts function to be used in other parts of the application
+module.exports = seedProducts;
