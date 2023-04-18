8e20d576284061f3a8893d3a9676b2dae371a5dd
Harold Gonzales <haroldgonzales@yahoo.com>
2023-04-17 23:16:34 -0700

    Created Models/\Tag.js

---
+++ models/Tag.js
@@ -0,0 +1,33 @@
+// Import necessary dependencies and modules
+const { Model, DataTypes } = require('sequelize');
+
+// Import the database connection configuration
+const sequelize = require('../config/connection');
+
+// Create a Tag class that extends Sequelize Model
+class Tag extends Model {}
+
+// Initialize Tag model with its attributes and configuration options
+Tag.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    tag_name: {
+      type: DataTypes.STRING,
+    },
+  },
+  {
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'tag',
+  }
+);
+
+// Export the Tag model to be used in other parts of the application
+module.exports = Tag;
